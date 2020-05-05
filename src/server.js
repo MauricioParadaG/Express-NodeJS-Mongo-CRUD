35d29fdb5ea3a025911e8b350069984a88c3b0ed
MauricioParadaG <62025899+MauricioParadaG@users.noreply.github.com>
2020-05-05 20:08:35 +0200

    Configuration of a template engine call express-handlebar
    Configuration of a template engine call express-handlebar

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const exphbs  = require('express-handlebars');
 const path = require ('path');
 
 // Initializations
@@ -6,7 +7,14 @@ const app = express();
 
 // Settings
 app.set("port", process.env.PORT || 4000);
-app.set('views', path.join(__dirname + 'views'));
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
 
 // Middlewares
 app.use(express.urlencoded({extended: false}));
@@ -15,11 +23,11 @@ app.use(express.urlencoded({extended: false}));
 
 // Routes
 app.get('/', (req, res) => {
-    res.send('Server is working?????')    
+    res.render('index');
 })
 
 // Static Files
-app.use(express.static(path.join(__dirname + 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
     
 
