5e116c01ee13397c78f4cc8237f56a2be8d36183
MauricioParadaG <62025899+MauricioParadaG@users.noreply.github.com>
2020-05-09 16:17:19 +0200

    I completed the Sigup, with messages, encrypted password and error alert for same email problems with password and more
    I completed the Sigup, with messages, encrypted password and error alert for same email problems with password and more

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -7,8 +7,33 @@ usersController.renderSignUpForm = (req, res) => {
     res.render('users/signup');
 };
 
-usersController.signup = (req, res) => {
-    res.send('signup');
+usersController.signup = async (req, res) => {
+    const errors = [];
+    const {name, email, password, confirm_password} = req.body;
+    if(password != confirm_password) {
+        errors.push({text: 'The Password do not match'});
+        req.flash('error_msg', 'Wrong password');
+    }
+    if(password.length < 5) {
+        errors.push({text: 'The Password must be at least 5 characters'});
+    }
+    if(errors.length > 0){
+        res.render('users/signup', {errors, name, email});
+    } else{
+        // Checking if email is repeated
+        const emailUser = await User.findOne({email: email});
+        if(emailUser){
+            req.flash('error_msg', 'The email already exist');
+            res.redirect('/users/signup');
+        } else{
+        // saving to database    
+        const newUser = new User({name: name, email: email, password: password});
+        newUser.password = await newUser.encryptPassword(password);
+        await newUser.save();
+        req.flash('success_msg', 'Registration was Sucessfull');
+        res.redirect('/users/signin');
+        }
+    }
 };
 
 // Sign in or Loging and form
