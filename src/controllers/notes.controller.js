f8890cc88d129fc4be566834a06d351f9806a2c8
MauricioParadaG <62025899+MauricioParadaG@users.noreply.github.com>
2020-05-10 13:14:40 +0200

    Showing the notes only for the user that created the now, and protecting the edit link from other users
    Showing the notes only for the user that created the now, and protecting the edit link from other users

--- src/controllers/notes.controller.js
+++ src/controllers/notes.controller.js
@@ -4,27 +4,33 @@ const Note = require('../models/Note');
 
 // New note
 notesController.renderNoteForm = (req, res) => {
+    console.log(req.user);
     res.render('notes/new-note');
 };
 
 notesController.createNewNote = async (req, res) => {
     const {title, description} = req.body;
     const newNote = new Note({title: title, description: description});
+  // Adding the user to the new note before save
+    newNote.user = req.user.id;
     await newNote.save();
     req.flash('success_msg', 'Note Added Sucessfully');
-
     res.redirect('/notes');
 };
 
 // Get All notes
 notesController.renderNotes = async (req, res) => {
-    const notes = await Note.find().lean();
+    const notes = await Note.find({user: req.user.id}).sort({createdAt:'desc'}).lean();
     res.render('notes/all-notes', {notes});
 };
 
 // Edit note
 notesController.renderEditForm = async (req, res) => {
     const note = await Note.findById(req.params.id).lean();
+    if(note.user != req.user.id){
+        req.flash('error_msg', 'This page does not exist')
+        return res.redirect('/notes');
+    }
     res.render('notes/edit-note', {note});
 };
 
